5d8382a10b59ad1f3c1f9a99678dc9bf8d972398
Charles Wong <jrwangnz@live.com>
2016-06-17 09:45:44 +0800

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,13 @@
+$(function(){
+	
+	$('.socials').mobilyblocks();
+	
+	$('.nature').mobilyblocks({
+		trigger: 'hover',
+		direction: 'counter',
+		duration:600,
+		zIndex:100,
+		widthMultiplier:1.5
+	});
+	
+});
