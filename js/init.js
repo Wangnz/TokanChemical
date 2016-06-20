d5d1a026139e5bc3da5b56cf2c3e84f61f465886
wangrex <503431920@qq.com>
2016-06-20 22:51:36 +0800

    Add about us

--- js/init.js
+++ js/init.js
@@ -10,4 +10,13 @@ $(function(){
 		widthMultiplier:1.5
 	});
 	
+	$.ajax({
+		type: "get",
+		data: {categoryName: "关于我们"},
+		url: "http://localhost:49566/api/HomeAPI/getList",
+		success: function(data) {
+			console.log(data);
+			$("#owl-demo").find(".fist-slide").html(template("aboutUS",data));
+		}
+	});
 });
